ba35d53128ffd335757941f0074c14db8c66c27c
AlexThe5th <afinlayv@gmail.com>
2020-07-21 16:24:05 -0400

    split functions, add direct load from lichess server, format DataFrame

--- chess_analysis.py
+++ chess_analysis.py
@@ -2,37 +2,62 @@
 import pandas as pd
 import chess.pgn
 from stockfish import Stockfish as sf
+import lichess.api
+from lichess.format import SINGLE_PGN
+
+USERNAME = 'AlexTheFifth'
+FILENAME = 'lichess_{}.pgn'.format(USERNAME)
+VERBOSE = False
+
+#load lichess data from lichess.org
+def load_user_data(name):
+    '''
+    load user data
+    '''
+    print('Loading user data...')
+    user = lichess.api.user(name)
+    user_data = user['perfs']
+    print(user_data)
+
+
+def save_game_data(file):
+    '''
+    load game data
+    '''
+    print('Loading game data... (this might take a while)')
+    pgn = lichess.api.user_games(USERNAME, format=SINGLE_PGN)
+    with open(FILENAME, 'w') as f:
+        print('.', end = '')
+        f.write(pgn)
 
 
 
-#load lichess PGN
+def load_game_data():
+    pgn = open(FILENAME)
+    result = {}
+    i = 0
+    while True:
+        i += 1
+        game = chess.pgn.read_game(pgn)
+        if game is None:
+            break
 
-pgn = open('lichess_AlexTheFifth.pgn')
+        headers = dict(game.headers)
+        headers["Moves"] = game.board().variation_san(game.mainline_moves())
 
-# make list of games
-game_lst = []
+        result["Game{}".format(i)] = headers
 
+    verbose(result)
 
+    df = pd.DataFrame.from_dict(data = result).transpose()
+    verbose(df)
+    return df
 
-# format games as dict
-result = {}
-i = 0
-while True:
-    i += 1
-    game = chess.pgn.read_game(pgn)
-    if game is None:
-        break
+def verbose(message, data):
+    delimiter = '\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'
+    print(delimiter, message, delimiter, data)
 
-    headers = dict(game.headers)
-    headers["Moves"] = game.board().variation_san(game.mainline_moves())
-
-    result["Game{}".format(i)] = headers
-
-print(result)
-
-# create pandas DataFrame
-
-df = pd.DataFrame.from_dict(data = result)
-
-
-print(df)
+def main():
+    load_user_data(USERNAME)
+    save_game_data(FILENAME)
+    game_data = load_game_data(FILENAME)
